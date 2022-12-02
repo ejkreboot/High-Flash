0616f6651e792922410e9b26f496c21f901da5dc
Eric Kort <eric@ericjkort.com>
2022-12-02 14:24:01 -0500

    flash object and studying

--- cards.js
+++ cards.js
@@ -1,4 +1,4 @@
-import { Sequelize, Model, DataTypes } from 'sequelize';
+import { Sequelize, Op, DataTypes } from 'sequelize';
 import { nanoid } from 'nanoid'
 
 export function Cards(persist = true, path = "../database.sqlite") {
@@ -21,14 +21,37 @@ export function Cards(persist = true, path = "../database.sqlite") {
     }
 
     const Card = sequelize.define('Card', {
-        front: DataTypes.TEXT,
-        back: DataTypes.TEXT,
-        category: DataTypes.TEXT,
         uuid: {
             type: DataTypes.UUID,
             primaryKey: true
-        }
+        },
+        front: DataTypes.TEXT,
+        back: DataTypes.TEXT,
+        category: DataTypes.TEXT
     });
+
+    /*
+     * The Flash model keeps track of each users mastery 
+     * of each card so that repetition intervals may be 
+     * adjusted accordingly.
+     */
+    const Flash = sequelize.define('Flash', {
+        uuid: {
+            type: DataTypes.UUID,
+            primaryKey: true
+        },
+        card: DataTypes.UUID,
+        user: DataTypes.UUID,
+        n: DataTypes.NUMBER,
+        efactor: DataTypes.NUMBER,
+        interval: DataTypes.NUMBER,
+        category: DataTypes.TEXT
+    });
+
+    async function sync_all() {
+        await Card.sync();
+        await Flash.sync();
+    }
 
     async function add_card(front, back, category) {
         await Card.sync();
@@ -91,6 +114,190 @@ export function Cards(persist = true, path = "../database.sqlite") {
         return card;
     }  
 
+    /*
+     * STUDY & SCORING FUNCTIONS
+     */
+
+    async function check_user(id, category) {
+        await sync_all();
+        let flashes = await Flash.findAll({
+            attributes: ['uuid'],
+            where: {
+                user: id
+            }
+        })
+        let cards = await Card.findAll({
+            attributes: ['uuid'],
+            where: {
+                category: category
+            }
+        })
+
+        if(flashes.length == 0 || flashes.length != cards.length) {
+            return(false)
+        } else {
+            return(true)
+        }
+    }
+
+    /*
+     * count the active cards for the specified user_id and category
+     */
+    async function active_cards(user_id, category) {
+        await sync_all();
+        let flashes = await Flash.findAll({
+            attributes: ['uuid'],
+            where: {
+                user: user_id,
+                category: category,
+                interval: { [Op.gt]: -1}
+            }
+        })
+        return(flashes.length)
+    }
+
+    /*
+     * count the active cards for the specified user_id and category
+     */
+    async function active_card_count(user_id, category) {
+        await sync_all();
+        let flashes = await Flash.findAll({
+            attributes: ['uuid'],
+            where: {
+                user: user_id,
+                category: category,
+                interval: { [Op.gt]: -1}
+            }
+        })
+        return(flashes.length)
+    }
+
+    /*
+     * count the inactive cards for the specified user_id and category
+     */
+    async function inactive_card_count(user_id, category) {
+        await sync_all();
+        let flashes = await Flash.findAll({
+            attributes: ['uuid'],
+            where: {
+                user: user_id,
+                category: category,
+                interval: -1
+            }
+        })
+        return(flashes.length)
+    }
+
+    /*
+     * make sure all the cards for this category are in the user's study history
+     * creating new entries where needed
+     */
+    async function init_study(user_id, category) {
+        await sync_all();
+        let cards = await Card.findAll({
+            attributes: ['uuid'],
+            where: {
+                category: category
+            }
+        })
+        let flashes = await Flash.findAll({
+            attributes: ['uuid'],
+            where: {
+                user: user_id,
+                category: category
+            }
+        })
+
+        if(cards.length > flashes.length) {
+            for (const c of cards) {
+                let flash = await Flash.findOne(
+                {
+                    attributes: ['uuid'],
+                    where: {
+                        user: user_id,
+                        card: c.uuid
+                    }
+                })
+                if(flash == null) {
+                    await init_flash(user_id, c.uuid);
+                }
+            }
+        }    
+        return;
+    }
+
+    /*
+     * intialize card for studying by this user. 
+     * private function.
+     */
+    async function init_flash(user_id, card_id, score = null) {
+        let interval = 0;
+        let count = 0;
+        let flash = await Flash.findOne({
+            attributes: ['n', 'efactor', 'interval'],
+            where: {
+                user: user_id,
+                card: card_id
+            }
+        })
+        if(flash== null) {
+            // this card is not yet setup for this user. Initialize.
+            let uuid = nanoid();
+            let card = await Card.findOne({
+                attributes: ['category'],
+                where: { 
+                    uuid: card_id
+                }
+            })
+            await Flash.create({
+                uuid: uuid,
+                card: card_id,
+                user: user_id,
+                n: 0.0,
+                efactor: 2.5,
+                interval: -1, // card is not active yet
+                category: card.category               
+            })
+        } 
+        return;
+    }
+
+    /*
+     * Update interval and efactor based on study score 
+     */
+    async function study(user_id, card_id, score) {
+        let sr = null;
+        const flash = await Flash.findOne({
+            attributes: ['n', 'efactor', 'interval'],
+            where: {
+                user: user_id,
+                card: card_id
+            }
+        })
+        if(flash== null) {
+            throw Error ('cannot study unitialized card (cards.js line 279)')
+        } else {
+            const previous = { n: flash.n, 
+                               efactor: flash.efactor, 
+                               interval: flash.interval };
+            const evaluation = { score: score };
+            sr = sm2(previous, evaluation);
+            await Flash.update(
+                { 
+                    n: sr.n,
+                    efactor: sr.factor,
+                    interval: sr.interval
+                },
+                { 
+                    where: { 
+                        card: card_id,
+                        user: user_id 
+                    }
+                });
+        }
+        return(sr);
+    }
+
     return {
       add_card: add_card,
       delete_card: delete_card,
@@ -98,6 +305,42 @@ export function Cards(persist = true, path = "../database.sqlite") {
       get_categories: get_categories,
       get_category: get_category,
       get_all: get_cards,
-      update_card: update_card
+      init_study: init_study,
+      update_card: update_card,
+      study: study,
+      active_card_count: active_card_count,
+      inactive_card_count: inactive_card_count
     }
+}
+
+
+/**
+ * This is the SM-2 algorithm from SuperMemo. 
+ *
+ * See https://www.supermemo.com/en/archives1990-2015/english/ol/sm2
+ */
+ function sm2(previous, evaluation) {
+    var n, efactor, interval
+
+    if (previous == null) {
+        previous = { n: 0, efactor: 2.5, interval: 0.0 }
+    }
+
+    efactor = Math.max(1.3, previous.efactor + (0.1 - (5 - evaluation.score) * (0.08+(5 - evaluation.score)*0.02)))
+
+    if (evaluation.score < 3) {
+        n = 0
+        interval = 1
+    } else {
+        n = previous.n + 1
+
+        if (previous.n == 0) {
+            interval = 1
+        } else if (previous.n == 1) {
+            interval = 6
+        } else {
+            interval = Math.ceil(previous.interval * efactor)
+        }
+    }
+    return {n, efactor, interval}
 }
--- tests/cards.test.js
+++ tests/cards.test.js
@@ -1,26 +1,30 @@
 import { Cards } from "../cards.js";
 import { strict as assert } from 'assert';
+import pkg from 'csvtojson';
+const { csv } = pkg;
+
+async function load_cards() {
+    const cards=await(csv().fromFile("tests/cards.csv"));
+    return(cards);
+}
+let c = new Cards(false);
 
 describe("Database functions", function() {
-    let c = new Cards(false);
     it("should be able to create a database and add cards", async function() {
-        await c.add_card("a", "b", "category 1") ;
-        await c.add_card("c", "d", "category 1") ;
-        await c.add_card("d", "e", "category 2") ;
-        await c.add_card("f", "g", "category 2") ;
-        await c.add_card("h", "i", "category 3") ;
+        let tc = await(load_cards());
+        tc.forEach(async function(a) {await c.add_card(a.Front, a.Back, a.Category)});
         let cards = await c.get_all();
-        assert.equal(cards.length, 5);
+        assert.equal(cards.length, 30);
     });
 
     it("should be able to retrieve card fronts", async function() {
         let cards = await c.get_all()
-        assert.equal(cards[1].front, "c");
+        assert.equal(cards[1].front, "What MRI imaging modality is represented by this image:");
     });
 
     it("should be able to retrieve card backs", async function() {
         let cards = await c.get_all()
-        assert.equal(cards[1].back, "d");
+        assert(cards[1].back.match('^T2 FLAIR.*'));
     });
 
     it("should be able to retrieve cards by uuid", async function() {
@@ -35,17 +39,58 @@ describe("Database functions", function() {
         const uuid = cards[0].uuid;
         await(c.delete_card(uuid));
         cards = await c.get_all();
-        assert.equal(cards.length, 4);
+        assert.equal(cards.length, 29);
     });
 
     it("should be able to retrieve categories", async function() {
         let cats = await c.get_categories();
-        assert.equal(cats.length, 3);
-        assert.equal(cats[1], "category 2");
+        assert.equal(cats.length, 2);
+        assert.equal(cats[1], "Endocrinology");
     });
 
     it("should be able to retrieve cards in a category", async function() {
-        let cards = await c.get_category("category 2");
-        assert.equal(cards.length, 2);
+        let cards = await c.get_category("Neurology");
+        assert.equal(cards.length, 17);
     });
 });
+
+describe("Study functions", function() {
+    it("should be able to initialize study for specified user", async function() {
+        let a = await c.active_card_count("1", "Neurology")
+        let i = await c.inactive_card_count("1", "Neurology")
+        assert.equal(a, 0)
+        assert.equal(i, 0)
+        await c.init_study("1", "Neurology")
+        a = await c.active_card_count("1", "Neurology")
+        i = await c.inactive_card_count("1", "Neurology")
+        assert.equal(a, 0)
+        assert.equal(i, 17)
+
+    });
+    it("should be able to add newly added card to user flash collection", async function() {
+        await c.add_card("A new card front", "A new card back", "Neurology");
+        let i = await c.inactive_card_count("1", "Neurology")
+        assert.equal(i, 17)
+        await c.init_study("1", "Neurology")
+        i = await c.inactive_card_count("1", "Neurology")
+        assert.equal(i, 18)
+    });
+    it("should be compute intervals after studying", async function() {
+        const cards = await c.get_category("Neurology")
+        let sr = await c.study("1", cards[0].uuid, 4)
+        assert.equal(sr.efactor, 2.5)
+        assert.equal(sr.interval, 1)
+
+        sr = await c.study("1", cards[0].uuid, 5)
+        assert.equal(sr.efactor, 2.6)
+        assert.equal(sr.interval, 6)
+
+        sr = await c.study("1", cards[0].uuid, 3)
+        sr = await c.study("1", cards[0].uuid, 1)
+        assert.equal(sr.efactor, 1.96)
+        assert.equal(sr.interval, 1)
+    });
+
+});
+
+
